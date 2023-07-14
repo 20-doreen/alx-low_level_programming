0d9f415c5a702cdab9062c142dcdf4a29e1d3946
20-doreen <doreenmemoh30@gmail.com>
2023-07-14 13:54:55 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main -  prints all possible different combinations of two digits
+ *
+ * Returns: 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 1 + 1; j < 10; j++)
+		{
+			putchar ('0' + i);
+			putchar ('0' + j);
+
+if (i < 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+		}
+	}
+putchar('\n');
+return (0);
+}
