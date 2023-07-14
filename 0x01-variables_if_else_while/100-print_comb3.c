dadbe0f80c24ec22fc7a628b1e24173800c5339a
20-doreen <doreenmemoh30@gmail.com>
2023-07-14 13:57:47 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 8; i++)
 	{
 		for (j = 1 + 1; j < 10; j++)
 		{
 			putchar ('0' + i);
 			putchar ('0' + j);
 
-if (i < 9)
+if (i < 8)
 {
 	putchar(',');
 	putchar(' ');
