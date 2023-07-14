1096a53b8ef6b713df75c0e1ad745511df8e4b40
20-doreen <doreenmemoh30@gmail.com>
2023-07-14 09:46:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main -  program that prints alphabets in lower and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+
+        for (i = 0; i < 52; i++)
+        {
+                putchar(alph[i]);
+        }
+        putchar('\n');
+        return (0);
+}
