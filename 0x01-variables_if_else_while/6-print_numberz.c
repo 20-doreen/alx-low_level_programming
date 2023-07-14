484ca42affc730671c09d82b6358a9be534bd79f
20-doreen <doreenmemoh30@gmail.com>
2023-07-14 10:45:04 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main  -  prints numbers of base 10 starting from 0 using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
