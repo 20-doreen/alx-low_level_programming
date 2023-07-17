bab056221e7da70b3421d7e1cca753a767c26f0b
20-doreen <doreenmemoh30@gmail.com>
2023-07-17 09:57:35 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,7 @@
 /**
  * main -  Entry point
  *Description:  prints possible different combinations of two digits
- * Returns: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
