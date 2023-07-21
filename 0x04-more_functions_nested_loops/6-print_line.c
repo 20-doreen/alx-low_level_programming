5df8a67ba19e94b5e6f151e1142891813ffa1729
20-doreen <doreenmemoh30@gmail.com>
2023-07-21 16:08:58 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,17 +5,15 @@
  */
 void print_line(int n)
 {
+	int i;
+	
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}else
+	else
 	{
-		int i;
 
 		for (i = 1; i <= n; i++)
-		{
 			_putchar('_');
-		}
 		_putchar('\n');
 	}
 }
